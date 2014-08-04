41bd02bf29a8211a12141e749ece5d0356837a8f
J. García <jyo.garcia@yahoo.com>
2014-08-03 21:13:56 -0600

    vim: improve update-bundles.sh to generate buldle-list.txt

--- vim/update-bundles.sh
+++ vim/update-bundles.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
-export n=1
+BLIST="$PWD/bundle-list.txt"
+
+# Checking for list of bundles url to exist
+[ -e $BLIST ] || touch $BLIST
 
 echo "Bundle Updater v$VERSION"
 
+# This functions adds the url of the origin of a bundle git repository
+add_tolist(){ 
+    #$1: repo_url $2: repo_dir
+    echo $1 >> $BLIST && echo "Bundle $2 wasn't in list" &&\
+        echo "  URL: $1 added!"
+}
+
+
+
+export n=1
 for dir in ./*/ ; do 
     echo " ($n) Updating bundle $dir"
     cd $dir ;
 
     printf "\t>>> Running 'git fetch' in $PWD\n"
+    ORIGIN_URL=$(git remote show origin | grep Fetch | cut -c 14-) 
+    echo "descargando desde $ORIGIN_URL"
     git fetch &&\
     printf "\t>>> Fetch successful\n"
     cd ..;
     n=$((n+1))
+    grep -q $ORIGIN_URL $BLIST ||  add_tolist $ORIGIN_URL $dir
+    
 done
+
+
